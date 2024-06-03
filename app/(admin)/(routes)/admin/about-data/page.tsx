460320e37b7524bb51ef2e799eaa803d3edea241
SyedAdeebWebDesigning <syadeeb@student.iul.ac.in>
2024-06-03 05:53:55 +0530

    Day 18: Add TimeLineData component and logout functionality

--- app/(admin)/(routes)/admin/about-data/page.tsx
+++ app/(admin)/(routes)/admin/about-data/page.tsx
@@ -1,11 +1,26 @@
+"use client";
 import { getAboutPageData } from "@/actions/aboutpage.action";
 import { AdminHeading } from "@/components/admin/AdminHeading";
 import { AboutPageForm } from "@/components/admin/forms/AboutPageForm";
 import { AboutPageData } from "@prisma/client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const AboutPageData = async () => {
-	const aboutPageData: AboutPageData | null | any = await getAboutPageData();
+const AboutPageData = () => {
+	const [aboutPageData, setAboutPageData] = useState<any>(null);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const result = await getAboutPageData();
+				setAboutPageData(result);
+			} catch (error) {
+				console.error("Error fetching home page data:", error);
+			}
+		};
+
+		fetchData();
+	}, []);
+	
 	return (
 		<main>
 			<AdminHeading>About Page Data</AdminHeading>
