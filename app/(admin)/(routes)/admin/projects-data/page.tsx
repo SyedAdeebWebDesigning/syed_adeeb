5c539f73e3e4d5f0b7f80458f45168c1a0bb3cd2
SyedAdeebWebDesigning <syadeeb@student.iul.ac.in>
2025-02-27 00:36:27 +0530

    Update project page with delete functionality and navigation

--- app/(admin)/(routes)/admin/projects-data/page.tsx
+++ app/(admin)/(routes)/admin/projects-data/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import { getProjects } from "@/actions/projects.action";
+import { deleteProject, getProjects } from "@/actions/projects.action";
 import { AdminHeading } from "@/components/admin/AdminHeading";
 import HandleNoData from "@/components/admin/handleNoData";
 import { Projects } from "@prisma/client";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { BadgeCheck } from "lucide-react";
+import { BadgeCheck, ChevronRightCircle, Trash2 } from "lucide-react";
+import { toast } from "@/components/ui/use-toast";
+import { redirect } from "next/navigation";
 
 type Props = {};
 
@@ -54,37 +56,59 @@ const ProjectPage = (props: Props) => {
 			) : projectData && projectData.length > 0 ? (
 				<section>
 					<div className="max-h-[75vh] w-full mx-auto overflow-y-auto p-4 pr-0">
-						{projectData.map((project, index) => (
-							<Link
-								href={`/admin/projects-data/${project.id}`}
-								key={project.id}
-								className="block w-full bg-white rounded-lg shadow-md border-gray-200 dark:bg-[#363636] dark:hover:bg-[#262626] mb-4">
-								<div className="flex items-center p-4">
-									<div className="relative h-24 w-32 rounded-lg overflow-hidden mr-4">
-										<Image
-											src={project.imgUrl}
-											layout="fill"
-											objectFit="cover"
-											alt=""
+						{projectData.map((project, index) => {
+							const deleteThisProject = async () => {
+								await deleteProject(project.id);
+								fetchData();
+							};
+							return (
+								<div
+									key={project.id}
+									className="block w-full relative bg-white rounded-lg shadow-md border-gray-200 dark:bg-[#363636] dark:hover:bg-[#262626] mb-4">
+									<div className="absolute right-2 top-2 p-2 cursor-pointer">
+										<Trash2
+											className="bg-red-300 text-white rounded-full size-10 p-2 hover:scale-110"
+											onClick={() => deleteThisProject()}
 										/>
 									</div>
-									<div className="flex-1">
-										<h2 className="text-xl font-semibold text-gray-900 dark:text-white mb-2 flex items-center">
-											{index + 1}.
-											<p className="line-clamp-1 lg:line-clamp-2 ml-1">
-												{project.title}
+									<Link
+										href={`/admin/projects-data/${project.id}`}
+										className="absolute right-14 top-2 p-2 cursor-pointer">
+										<ChevronRightCircle
+											className="bg-blue-300 text-white rounded-full size-10 p-2 hover:scale-110"
+											onClick={() => deleteThisProject}
+										/>
+									</Link>
+									<div className="flex items-center p-4">
+										<div className="relative h-24 w-32 rounded-lg overflow-hidden mr-4">
+											<Image
+												src={project.imgUrl}
+												layout="fill"
+												objectFit="cover"
+												alt=""
+											/>
+										</div>
+										<div className="flex-1">
+											<h2 className="text-xl font-semibold text-gray-900 dark:text-white mb-2 flex items-center">
+												{index + 1}.
+												<p className="line-clamp-1 lg:line-clamp-2 ml-1">
+													{project.title}
+												</p>
+												{project.isFeatured && (
+													<BadgeCheck
+														className="text-green-500 ml-2"
+														size={24}
+													/>
+												)}
+											</h2>
+											<p className="text-gray-700 dark:text-gray-300 line-clamp-2">
+												{project.description}
 											</p>
-											{project.isFeatured && (
-												<BadgeCheck className="text-green-500 ml-2" size={24} />
-											)}
-										</h2>
-										<p className="text-gray-700 dark:text-gray-300 line-clamp-2">
-											{project.description}
-										</p>
+										</div>
 									</div>
 								</div>
-							</Link>
-						))}
+							);
+						})}
 					</div>
 				</section>
 			) : (
